803164b35235c0f4597625909d78b50025ed6402
Andrew Hayakawa <amac0183@gmail.com>
2018-07-08 16:30:25 -0400

    Added basic webpack configurations, styling, and the sections container

--- src/js/index.jsx
+++ src/js/index.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import '../sass/index.scss'
+
 const Index = () => {
   return (
-    <div>Yay!</div>
+    <div className='section-container'>
+    </div>
   )
 };
 
--- webpack.config.js
+++ webpack.config.js
@@ -2,20 +2,38 @@ const path = require('path');
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 
 const htmlPlugin = new HtmlWebPackPlugin({
-  template: path.join(__dirname, 'src/index.html'),
+  template: path.join(__dirname, 'src/html/index.html'),
   filename: './index.html'
 });
 
 module.exports = {
-  entry: path.join(__dirname, 'src/js/index.js'),
+  entry: path.join(__dirname, 'src/js/index.jsx'),
   module: {
     rules: [
       {
-        test: /\.js$/,
+        test: /\.scss$/,
+        use: [
+          {
+            loader: "style-loader"
+          },
+          {
+            loader: "css-loader"
+          },
+          {
+            loader: "sass-loader"
+          }
+        ]
+      },
+      {
+        test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader'
         }
+      },
+      {
+        test: /\.jpg$/,
+        use: ['url-loader']
       }
     ]
   },
