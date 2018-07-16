d2a884df9f6ed8ea4f2bbad61e2751677178f3ec
Andrew Hayakawa <amac0183@gmail.com>
2018-07-15 22:22:34 -0400

    Added data file

---
+++ src/js/data.js
@@ -0,0 +1,103 @@
+export const DATA = {
+  cover: {
+    name: 'Andrew Hayakawa',
+    title: 'Experienced Software Engineer'
+  },
+  skills: {
+    databases: [
+      {
+        name: 'GraphQL'
+      },
+      {
+        name: 'MySQL'
+      }
+    ],
+    languages: [
+      {
+        name: 'JavaScript'
+      },
+      {
+        name: 'SASS/CSS'
+      },
+      {
+        name:'C++'
+      }
+    ],
+    javascriptLibraries: [
+      {
+        name: 'React'
+      },
+      {
+        name: 'Redux/Redux Sagas'
+      },
+      {
+        name: 'Webpack 4'
+      },
+      {
+        name: 'Express 4.x'
+      },
+      {
+        name: 'Koa'
+      },
+      {
+        name: 'Babel'
+      },
+      {
+        name: 'Enzyme'
+      }
+    ],
+    tools: [
+      {
+        name: 'GitLab/Github'
+      },
+      {
+        name: 'Docker'
+      },
+      {
+        name: 'Kubernetes'
+      },
+      {
+        name: 'Amazon Web Services'
+      }
+    ]
+  },
+  experiences: {
+    activities: [
+    ],
+    education: [
+      {
+        degree: 'B.S. in Information Technology',
+        graduationDate: 200512,
+        school: 'Rensselaer Polytechnic Institute',
+        location: 'Troy, NY' 
+      }
+    ],
+    work: [
+      {
+        startDate: 200601,
+        endDate: 201507,
+        company: 'FactSet Research Systems, Inc.',
+        position: 'Senior Software Engineer',
+        summary: '',
+        details: []
+      },
+      {
+        startDate: 201601,
+        endDate: null,
+        company: 'LearnVest, Inc.',
+        position: 'Senior Software Engineer',
+        summary: '',
+        details: []
+      }
+    ]
+  },
+  profile: {
+    bio: '',
+    name: 'Andrew Hayakawa',
+    location: 'New York, NY',
+    social: {
+      linkedin: '',
+      twitter: ''
+    }
+  }
+}
