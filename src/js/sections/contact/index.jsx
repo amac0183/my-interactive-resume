9f0a950a8ed81de811c1d8dd64a45cfc336b4e35
Andrew Hayakawa <amac0183@gmail.com>
2018-10-21 20:25:57 -0400

    Added content for the Contact section

--- src/js/sections/contact/index.jsx
+++ src/js/sections/contact/index.jsx
@@ -1,11 +1,27 @@
 import React from 'react';
 import {BaseSection} from '../base_section/index.jsx';
+import {ContactEntry} from './components/contact_entry/index.jsx';
 
 export const ContactSection = () => {
   return (
     <div id='contact'>
       <BaseSection title='Contact'>
-        <div>Contact</div>
+        <div className='contact-list'>
+          <ContactEntry 
+            openInNewTab
+            icon={<ion-icon name='logo-linkedin'/>}
+            url='http://www.linkedin.com/in/andrew-hayakawa-a6043317'
+          />
+          <ContactEntry
+            icon={<i class="material-icons">mail</i>}
+            url='mailto:isao.a.hayakawa@gmail.com'
+          />
+          <ContactEntry
+            openInNewTab
+            icon={<ion-icon name='logo-github' />}
+            url='https://github.com/amac0183'
+          />
+        </div>
       </BaseSection>
     </div>
   );
